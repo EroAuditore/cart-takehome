0cc8ba32d54ee43ce0712206a636ba70128db360
eroauditore <alfa.fenrir@gmail.com>
2022-11-23 16:03:11 -0600

    test for imported and basic tax

--- __tests__/Calculate.test.js
+++ __tests__/Calculate.test.js
@@ -1,4 +1,4 @@
-import { calculateBasicTax } from "../src/Calculate";
+import { calculateBasicTax, calculateImportTax } from "../src/Calculate";
 import Item from "./../src/Item";
 
 const testItem = {
@@ -8,11 +8,28 @@ const testItem = {
   hasImportTax: false,
 };
 
-describe("Calculate", () => {
-  it("return de 10% of the price of item that has basic tax", () => {
+const testItem2 = {
+  description: "imported bottle of perfume",
+  price: 27.99,
+  hasBasicTax: true,
+  hasImportTax: true,
+};
+
+describe("Calculate helper", () => {
+  it("calcualte only 10% of the price of item that has basic tax", () => {
     const cartitem = new Item(testItem);
     const basicTax = calculateBasicTax(cartitem);
+    const importTax = calculateImportTax(cartitem);
 
     expect(basicTax).toBe(1.5);
+    expect(importTax).toBe(0);
+  });
+  it("calculate the 10% and 5% of the price of item with basic and import tax", () => {
+    const cartitem = new Item(testItem2);
+    const basicTax = calculateBasicTax(cartitem);
+    const importTax = calculateImportTax(cartitem);
+
+    expect(basicTax).toBe(2.8);
+    expect(importTax).toBe(1.4);
   });
 });
